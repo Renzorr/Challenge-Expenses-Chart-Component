2dcd1ddf8e9204fd4be2d73f401d37ee70f673c1
renzorr <benaventer01@gmail.com>
2022-12-18 21:34:45 -0500

    add animation

---
+++ js/animation.js
@@ -0,0 +1,55 @@
+anime({
+  targets: ".round-1",
+  innerHTML: [0, `$${921.48}`],
+  round: 100,
+  duration: 1100,
+});
+
+anime({
+  targets: ".round-2",
+  innerHTML: [0, `$${478.33}`],
+  round: 100,
+  duration: 1100,
+});
+
+anime({
+  targets: ".round-3",
+  innerHTML: [0, `+ ${2.4}%`],
+  round: 100,
+  duration: 1100,
+});
+
+anime({
+  targets: ".bar_1",
+  height: "60px",
+});
+
+anime({
+  targets: ".bar_2",
+  height: "100px",
+});
+
+anime({
+  targets: ".bar_3",
+  height: "150px",
+});
+
+anime({
+  targets: ".bar_4",
+  height: "90px",
+});
+
+anime({
+  targets: ".bar_5",
+  height: "70px",
+});
+
+anime({
+  targets: ".bar_6",
+  height: "130px",
+});
+
+anime({
+  targets: ".bar_7",
+  height: "70px",
+});
--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
+// url
 const url = "../js/data.json";
-
+// components
 const days = document.querySelectorAll(".bar_day");
 const amounts = document.querySelectorAll(".bar_amount");
 const bars = document.querySelectorAll(".bar");
 
-async function Datajson() {
+const expensesChart = async () => {
   const response = await fetch(url);
   const data = await response.json();
-
-  console.log(data);
 
   days.forEach((element, i) => {
     element.innerHTML = data[i].day;
@@ -17,17 +16,20 @@ async function Datajson() {
   amounts.forEach((element, i) => {
     element.innerHTML = `$${data[i].amount}`;
   });
-}
+};
 
-for (let i = 0; i < bars.length; i++) {
-  for (let j = 0; j < amounts.length; j++) {
-    bars[i].addEventListener("mouseenter", () => {
-      amounts[i].classList.remove("inactive");
-    });
-    bars[i].addEventListener("mouseleave", () => {
-      amounts[i].classList.add("inactive");
-    });
+const amountHover = () => {
+  for (let i = 0; i < bars.length; i++) {
+    for (let j = 0; j < amounts.length; j++) {
+      bars[i].addEventListener("mouseenter", () => {
+        amounts[i].classList.remove("inactive");
+      });
+      bars[i].addEventListener("mouseleave", () => {
+        amounts[i].classList.add("inactive");
+      });
+    }
   }
-}
+};
 
-Datajson();
+expensesChart();
+amountHover();
